2caaaaba7f1abfc02744861fcd10f85674e9da1c
Jean-Dv <mrjunior127@gmail.com>
2022-07-24 14:48:34 -0500

    Changed folders for routes with version

--- api/home/routers/v1.ts
+++ api/home/routers/v1.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express'
 
-import { HomeHttpHandler } from './http'
+import { HomeHttpHandler } from '../http'
 
 export const homeRouter = Router()
 const homeHttpHandler = new HomeHttpHandler()
 
-homeRouter.route('/')
-  .get(homeHttpHandler.getPing)
+homeRouter.route('/').get(homeHttpHandler.getPing)

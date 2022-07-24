1d48b7af89a4b3d071201555fe334aee78db6d5f
Jean-Dv <mrjunior127@gmail.com>
2022-07-24 16:15:54 -0500

    Added service for create new workout

--- api/workout/service.ts
+++ api/workout/service.ts
@@ -1,4 +1,6 @@
 import { Workout } from '../../services/database/Workout'
+import DB from '../../services/database/db.json'
+import { NewWorkout } from './types'
 const workout = new Workout()
 export class WorkoutService {
   getAllWorkouts (): object {
@@ -10,8 +12,14 @@ export class WorkoutService {
     return ''
   }
 
-  createNewWorkout (): string {
-    return ''
+  createNewWorkout (newWorkout: NewWorkout): NewWorkout | string {
+    const isAlreadyAdded = DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1
+    if (isAlreadyAdded) {
+      return ''
+    }
+    DB.workouts.push(newWorkout)
+    workout.saveToDatabase(DB)
+    return newWorkout
   }
 
   updateOneWorkout (): string {

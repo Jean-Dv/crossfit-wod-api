91cb3e152b9c63a74f5141473e9123d15db44117
Jean-Dv <mrjunior127@gmail.com>
2022-07-24 15:49:59 -0500

    Added test for create new workout

--- __test__/workout.spec.ts
+++ __test__/workout.spec.ts
@@ -9,6 +9,34 @@ describe(`CRUD ${routePrefix}/workouts`, () => {
     expect(response.statusCode).toBe(200)
     expect(response.body.data[0]).toMatchObject({ name: 'Tommy V' })
   })
+
+  test('should return 200 and data with new workout', async () => {
+    const newWorkout = {
+      name: 'Core Buster',
+      mode: 'AMRAP 20',
+      equipment: [
+        'rack',
+        'barbell',
+        'abmat'
+      ],
+      exercises: [
+        '15 toes to bars',
+        '10 thrusters',
+        '30 abmat sit-ups'
+      ],
+      trainerTips: [
+        'Split your toes to bars into two sets maximum',
+        'Go unbroken on the thrusters',
+        'Take the abmat sit-ups as a chance to normalize your breath'
+      ]
+    }
+    const response = await request(appServer)
+      .post(`${routePrefix}/workouts`)
+      .set('Accept', 'application/json')
+      .send(newWorkout)
+    expect(response.statusCode).toBe(200)
+    expect(response.body.data).toContain({ name: 'Core Buster' })
+  })
 })
 
 afterAll(() => {

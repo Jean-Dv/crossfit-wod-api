f8d1ff16bb52e844fffec546c1fd2d4732e3cbc5
Jean-Dv <mrjunior127@gmail.com>
2022-07-24 14:48:00 -0500

    Delete @param without util

--- api/home/http.ts
+++ api/home/http.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express'
 
 export class HomeHttpHandler {
   /**
-     * @param {Request|any} req -> Request of client
      * @return {Response} res -> Response of server
      */
   getPing (_req: Request, res: Response): Response {

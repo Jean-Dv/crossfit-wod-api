4690d98068eb764af6e83b4c06a94d059cc259e4
Jean-Dv <mrjunior127@gmail.com>
2022-07-24 16:16:16 -0500

    Added validations for http

--- api/workout/http.ts
+++ api/workout/http.ts
@@ -21,10 +21,19 @@ export class WorkoutHttpHandler {
   }
 
   createNewWorkout (req: Request, res: Response): Response {
-    const message = workoutController.createNewWorkout()
+    const { body } = req
+    const { name, mode, equipment, exercises, trainerTips } = body
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!name || !mode || !equipment || !exercises || !trainerTips) {
+      return res.status(400).json({
+        ok: false,
+        message: 'Missing data'
+      })
+    }
+    const createNewWorkout = workoutController.createNewWorkout({ name, mode, equipment, exercises, trainerTips })
     return res.status(200).json({
       ok: true,
-      message: message
+      message: createNewWorkout
     })
   }
 

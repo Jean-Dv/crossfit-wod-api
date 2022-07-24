aff633d02e51ff5a0a09c310bd308f6d178952f1
Jean-Dv <mrjunior127@gmail.com>
2022-07-24 16:15:10 -0500

    Added save to Database into service global to 'database'

--- services/database/Workout.ts
+++ services/database/Workout.ts
@@ -1,7 +1,14 @@
+import { writeFileSync } from 'fs'
 import DB from './db.json'
 
 export class Workout {
   getAllWorkouts (): object {
     return DB.workouts
+  }
+
+  saveToDatabase (DB: object): void {
+    writeFileSync('../../services/database/db.json', JSON.stringify(DB, null, 2), {
+      encoding: 'utf-8'
+    })
   }
 }

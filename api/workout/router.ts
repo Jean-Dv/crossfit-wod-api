d91f3aab403a3ce080fcab75a292eab4437690b4
Jean-Dv <mrjunior127@gmail.com>
2022-07-27 11:29:36 -0500

    Added cache for routes with cache (gets)

--- api/workout/router.ts
+++ api/workout/router.ts
@@ -1,17 +1,19 @@
 import { Router } from 'express'
+import apicache from 'apicache'
 
 import { WorkoutHttpHandler } from './http'
 import { RecordHttpHandler } from '../record/http'
 
 export const workoutRouter = Router()
+const cache = apicache.middleware
 const recordHttpHandler = new RecordHttpHandler()
 const workoutHttpHandler = new WorkoutHttpHandler()
 
 workoutRouter.get('/', workoutHttpHandler.getAllWorkouts)
 
-workoutRouter.get('/:workoutId', workoutHttpHandler.getOneWorkout)
+workoutRouter.get('/:workoutId', cache('2 minutes'), workoutHttpHandler.getOneWorkout)
 
-workoutRouter.get('/:workoutId/records', recordHttpHandler.getRecordForWorkout)
+workoutRouter.get('/:workoutId/records', cache('2 minutes'), recordHttpHandler.getRecordForWorkout)
 
 workoutRouter.post('/', workoutHttpHandler.createNewWorkout)
 
